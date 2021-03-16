0540d5aeea98da45fc50db0e9532963529619ae5
Ramin Suleymanov <ramin.suleymanov666@gmail.com>
2021-03-16 12:19:24 +0400

    third commit

---
+++ src/components/AddForm.js
@@ -0,0 +1,55 @@
+import { Form,Button } from 'react-bootstrap';
+import {EmployeeContext} from '../context/EmployeeContext';
+import {useContext, useState} from "react";
+const AddForm=()=>{
+    const {addEmployee}=useContext(EmployeeContext);
+    const[name,setName]=useState("");
+    const[email,setEmail]=useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        addEmployee(name,email,address,phone);
+    }
+    return(
+       <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder="Name *"
+              required
+               />
+          </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Email *"
+                    required
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    as="textarea"
+                    value={address}
+                    onChange={e => setAddress(e.target.value)}
+                    placeholder="Address "
+                    rows={3}
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
+                    placeholder="Phone "
+                />
+            </Form.Group>
+            <Button variant="success" type="submit" block>Add new employee</Button>
+       </Form>
+    );
+}
+export default AddForm;
